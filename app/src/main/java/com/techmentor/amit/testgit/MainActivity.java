bffc19b9c84f3ac1c4c62cfef1b001f9c7edb8b9
tripathiamitkumar <er.amitkumartripathi@gmail.com>
2018-07-24 19:08:05 +0530

    This is my initial test connection to git

--- app/src/main/java/com/techmentor/amit/testgit/MainActivity.java
+++ app/src/main/java/com/techmentor/amit/testgit/MainActivity.java
@@ -10,7 +10,7 @@ import android.view.Menu;
 import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
-
+// this is my test commit to git
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
